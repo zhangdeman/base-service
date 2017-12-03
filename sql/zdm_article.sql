9521f5c4b9452138c0c98be5de5b6a99620c2303
张德满 <2215508028@qq.com>
2017-12-03 21:03:22 +0800

    update sql

--- sql/zdm_article.sql
+++ sql/zdm_article.sql
@@ -5,7 +5,7 @@ CREATE TABLE `zdm_article` (
   `html_content` TEXT NOT NULL DEFAULT '' COMMENT '带格式的文章内容',
   `text_content` TEXT NOT NULL DEFAULT '' COMMENT '纯文本文章内容',
   `parent_kind` TINYINT NOT NULL DEFAULT 0 COMMENT '父级分类',
-  `son_kind`  TINYINT NOT NULL DEFAULT 0 COMMENT '子分类',
+  `son_kind`  INT(6) NOT NULL DEFAULT 0 COMMENT '子分类',
   `create_ip` VARCHAR(20) NOT NULL DEFAULT '' COMMENT '发布文章的客户端IP',
   `create_time` BIGINT(20) NOT NULL DEFAULT unix_timestamp(now()) COMMENT '创建时间',
   `db_time` bigint(20) NOT NULL DEFAULT  unix_timestamp(now())  COMMENT '数据上一次被更新时间，业务无关',
